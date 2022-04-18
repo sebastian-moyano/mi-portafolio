a7a41787b05eb541dbf614c27deef64bd6fc4f33
sebastian-moyano <sebastian.moyano.seba@gmail.com>
2022-04-18 11:05:11 -0300

    lluvia para respon

--- index.js
+++ index.js
@@ -31,7 +31,27 @@ else{
   
 }
 });
-  
+ // animacion de lluevia
+ function lluvia(){
+  let monto = 70;
+  let body = document.querySelector("body");
+  let i = 0;
+  while (i < monto) {
+      let soltar = document.createElement("i");
+      let dimencion = Math.random() * 5;
+      let posX = Math.floor(Math.random() * window.innerWidth);
+      let delay = Math.random() * -20;
+      let duracion = Math.random() * 5;
+
+      soltar.style.width = 0.2 + dimencion + "px";
+      soltar.style.left = posX + "px";
+      soltar.style.animationDelay = delay+"s";
+      soltar.style.animationDuration = 1 + duracion + "s";
+      body.appendChild(soltar);
+      i++
+  }
+}
+lluvia(); 
 
   
 
